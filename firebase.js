340ff8465698e221a1ac86853dffc1e8c01a0254
Deadalt <117910136+Deadalt@users.noreply.github.com>
2025-07-27 17:48:40 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase configuration - include this in all pages that need Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBj912pLGQg06hUXcJi5XSwAWJailUWcZs",
+  authDomain: "arabjamatjamnagar.firebaseapp.com",
+  projectId: "arabjamatjamnagar",
+  storageBucket: "arabjamatjamnagar.firebasestorage.app",
+  messagingSenderId: "257628939713",
+  appId: "1:257628939713:web:8eab9357803ee72c6cbb40",
+  measurementId: "G-4SC1KSRV8H"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
